ab32a7ca63e7983bdcbd496b3645b9c9bcc41c6f
Irvan Aditya <iaditya9391@gmail.com>
2013-05-14 11:58:09 +0700

    testing #4
    home lumayan jadi

--- main.js
+++ main.js
@@ -1,191 +1,225 @@
-var selectednavbutton;
-var selectedsubnavbutton;
+var loadedPage;
 
 $(document).ready(function() {
-	$('#main').load('home.html');
+	$('#main').load('home1.html');
+	loadedPage = 1;
 	// selectednavbutton = '1';
 	// selectedsubnavbutton = '1';
 	// updateNavigation();
 });
 
 function loadPage(pagenum) {
-	var sliderdiv;
-	var pos_x;
-	if (pagenum == '1' || pagenum == '2' || pagenum == '6') {
+	if (pagenum != loadedPage) {
+		var pagename;
 		switch (pagenum) {
-			case ('1') : {
-				if (selectednavbutton != '1') {
-					pos_x = - $(window).width();
-					sliderdiv = $('<div id="slider" style="left:' + pos_x + 'px"></div>');
-					sliderdiv.load('home.html');
-					$('#main').after(sliderdiv);
-					$('#main').animate({
-						left:'+=' + $(window).width()
-					},800);
-					$('#slider').animate({
-						opacity:'1.0',
-						left:'+=' + $(window).width()
-					},800,function() {
-						$('#main').remove();
-						$('#slider').attr('id','main');
-						$('#main').removeAttr('style');
-					});
-				}
-				selectedsubnavbutton = '1';
+			case (1) : {
+				pagename = 'home1.html';
 				break;
 			}
-			case ('2') : {
-				if (selectedsubnavbutton != '3') {
-					if (pagenum < selectedsubnavbutton) {
-						pos_x = - $(window).width();
-					}
-					else {
-						pos_x = $(window).width();
-					}
-					sliderdiv = $('<div id="slider" style="left:' + pos_x + 'px"></div>');
-					sliderdiv.load('test1-1.html');
-					$('#main').after(sliderdiv);
-					if (pagenum < selectedsubnavbutton) {
-						$('#main').animate({
-							left:'+=' + $(window).width()
-						},800);
-						$('#slider').animate({
-							opacity:'1.0',
-							left:'+=' + $(window).width()
-						},800,function() {
-							$('#main').remove();
-							$('#slider').attr('id','main');
-							$('#main').removeAttr('style');
-						});
-					}
-					else {
-						$('#main').animate({
-							left:'-=' + $(window).width()
-						},800);
-						$('#slider').animate({
-							opacity:'1.0',
-							left:'-=' + $(window).width()
-						},800,function() {
-							$('#main').remove();
-							$('#slider').attr('id','main');
-							$('#main').removeAttr('style');
-						});
-					}
-					selectedsubnavbutton = '3';
-				}
+			case (2) : {
+				pagename = 'home2.html';
 				break;
 			}
-			case ('6') : {
-				if (selectedsubnavbutton != '7') {
-					if (pagenum < selectedsubnavbutton) {
-						pos_x = - $(window).width();
-					}
-					else {
-						pos_x = $(window).width();
-					}
-					sliderdiv = $('<div id="slider" style="left:' + pos_x + 'px"></div>');
-					sliderdiv.load('test2-1.html');
-					$('#main').after(sliderdiv);
-					if (pagenum < selectedsubnavbutton) {
-						$('#main').animate({
-							left:'+=' + $(window).width()
-						},800);
-						$('#slider').animate({
-							opacity:'1.0',
-							left:'+=' + $(window).width()
-						},800,function() {
-							$('#main').remove();
-							$('#slider').attr('id','main');
-							$('#main').removeAttr('style');
-						});
-					}
-					else {
-						$('#main').animate({
-							left:'-=' + $(window).width()
-						},800);
-						$('#slider').animate({
-							opacity:'1.0',
-							left:'-=' + $(window).width()
-						},800,function() {
-							$('#main').remove();
-							$('#slider').attr('id','main');
-							$('#main').removeAttr('style');
-						});
-					}
-					selectedsubnavbutton = '7';
-				}
+			case (3) : {
+				pagename = 'home3.html';
 				break;
 			}
 		}
-		selectednavbutton = pagenum;
-	}
-	else {
-		if (selectedsubnavbutton != pagenum) {
-			if (pagenum < selectedsubnavbutton) {
-				pos_x = - $(window).width();
-			}
-			else {
-				pos_x = $(window).width();
-			}
-			sliderdiv = $('<div id="slider" style="left:' + pos_x + 'px"></div>');
-			switch (pagenum) {
-				case ('3') : {
-					sliderdiv.load('test1-1.html');
-					selectednavbutton = '2';
-					break;
-				}
-				case ('4') : {
-					sliderdiv.load('test1-2.html');
-					selectednavbutton = '2';
-					break;
-				}
-				case ('5') : {
-					sliderdiv.load('test1-3.html');
-					selectednavbutton = '2';
-					break;
-				}
-				case ('7') : {
-					sliderdiv.load('test2-1.html');
-					selectednavbutton = '3';
-					break;
-				}
-				case ('8') : {
-					sliderdiv.load('test2-2.html');
-					selectednavbutton = '3';
-					break;
-				}
-			}
-			$('#main').after(sliderdiv);
-			if (pagenum < selectedsubnavbutton) {
-				$('#main').animate({
-					left:'+=' + $(window).width()
-				},800);
-				$('#slider').animate({
-					opacity:'1.0',
-					left:'+=' + $(window).width()
-				},800,function() {
-					$('#main').remove();
-					$('#slider').attr('id','main');
-					$('#main').removeAttr('style');
-				});
-			}
-			else {
-				$('#main').animate({
-					left:'-=' + $(window).width()
-				},800);
-				$('#slider').animate({
-					opacity:'1.0',
-					left:'-=' + $(window).width()
-				},800,function() {
-					$('#main').remove();
-					$('#slider').attr('id','main');
-					$('#main').removeAttr('style');
-				});
-			}
-			selectedsubnavbutton = pagenum;
+		if (pagenum < loadedPage) {
+			loadFromLeft(pagename);
 		}
+		else {
+			loadFromRight(pagename);
+		}
+		loadedPage = pagenum;
 	}
-	updateNavigation();
+	// if (pagenum == '1' || pagenum == '2' || pagenum == '6') {
+		// switch (pagenum) {
+			// case ('1') : {
+				// if (selectednavbutton != '1') {
+					// pos_x = - $(window).width();
+					// sliderdiv = $('<div id="slider" style="left:' + pos_x + 'px"></div>');
+					// sliderdiv.load('home.html');
+					// $('#main').after(sliderdiv);
+					// $('#main').animate({
+						// left:'+=' + $(window).width()
+					// },800);
+					// $('#slider').animate({
+						// opacity:'1.0',
+						// left:'+=' + $(window).width()
+					// },800,function() {
+						// $('#main').remove();
+						// $('#slider').attr('id','main');
+						// $('#main').removeAttr('style');
+					// });
+				// }
+				// selectedsubnavbutton = '1';
+				// break;
+			// }
+			// case ('2') : {
+				// if (selectedsubnavbutton != '3') {
+					// if (pagenum < selectedsubnavbutton) {
+						// pos_x = - $(window).width();
+					// }
+					// else {
+						// pos_x = $(window).width();
+					// }
+					// sliderdiv = $('<div id="slider" style="left:' + pos_x + 'px"></div>');
+					// sliderdiv.load('test1-1.html');
+					// $('#main').after(sliderdiv);
+					// if (pagenum < selectedsubnavbutton) {
+						// $('#main').animate({
+							// left:'+=' + $(window).width()
+						// },800);
+						// $('#slider').animate({
+							// opacity:'1.0',
+							// left:'+=' + $(window).width()
+						// },800,function() {
+							// $('#main').remove();
+							// $('#slider').attr('id','main');
+							// $('#main').removeAttr('style');
+						// });
+					// }
+					// else {
+						// $('#main').animate({
+							// left:'-=' + $(window).width()
+						// },800);
+						// $('#slider').animate({
+							// opacity:'1.0',
+							// left:'-=' + $(window).width()
+						// },800,function() {
+							// $('#main').remove();
+							// $('#slider').attr('id','main');
+							// $('#main').removeAttr('style');
+						// });
+					// }
+					// selectedsubnavbutton = '3';
+				// }
+				// break;
+			// }
+			// case ('6') : {
+				// if (selectedsubnavbutton != '7') {
+					// if (pagenum < selectedsubnavbutton) {
+						// pos_x = - $(window).width();
+					// }
+					// else {
+						// pos_x = $(window).width();
+					// }
+					// sliderdiv = $('<div id="slider" style="left:' + pos_x + 'px"></div>');
+					// sliderdiv.load('test2-1.html');
+					// $('#main').after(sliderdiv);
+					// if (pagenum < selectedsubnavbutton) {
+						// $('#main').animate({
+							// left:'+=' + $(window).width()
+						// },800);
+						// $('#slider').animate({
+							// opacity:'1.0',
+							// left:'+=' + $(window).width()
+						// },800,function() {
+							// $('#main').remove();
+							// $('#slider').attr('id','main');
+							// $('#main').removeAttr('style');
+						// });
+					// }
+					// else {
+						// $('#main').animate({
+							// left:'-=' + $(window).width()
+						// },800);
+						// $('#slider').animate({
+							// opacity:'1.0',
+							// left:'-=' + $(window).width()
+						// },800,function() {
+							// $('#main').remove();
+							// $('#slider').attr('id','main');
+							// $('#main').removeAttr('style');
+						// });
+					// }
+					// selectedsubnavbutton = '7';
+				// }
+				// break;
+			// }
+		// }
+		// selectednavbutton = pagenum;
+	// }
+	// else {
+		// if (selectedsubnavbutton != pagenum) {
+			// if (pagenum < selectedsubnavbutton) {
+			// }
+			// else {
+			// }
+			// switch (pagenum) {
+				// case ('3') : {
+					// sliderdiv.load('test1-1.html');
+					// selectednavbutton = '2';
+					// break;
+				// }
+				// case ('4') : {
+					// sliderdiv.load('test1-2.html');
+					// selectednavbutton = '2';
+					// break;
+				// }
+				// case ('5') : {
+					// sliderdiv.load('test1-3.html');
+					// selectednavbutton = '2';
+					// break;
+				// }
+				// case ('7') : {
+					// sliderdiv.load('test2-1.html');
+					// selectednavbutton = '3';
+					// break;
+				// }
+				// case ('8') : {
+					// sliderdiv.load('test2-2.html');
+					// selectednavbutton = '3';
+					// break;
+				// }
+			// }
+			// if (pagenum < selectedsubnavbutton) {
+			// }
+			// else {
+			// }
+			// selectedsubnavbutton = pagenum;
+		// }
+	// }
+	// updateNavigation();
+}
+
+function loadFromLeft(page) {
+	var pos_x = - $(window).width();
+	var sliderdiv = $('<div id="slider" style="left:' + pos_x + 'px"></div>');
+	sliderdiv.load(page,function() {
+		$('#main').after(sliderdiv);
+		$('#main').animate({
+			left:'+=' + $(window).width()
+		},800);
+		$('#slider').animate({
+			opacity:'1.0',
+			left:'+=' + $(window).width()
+		},800,function() {
+			$('#main').remove();
+			$('#slider').attr('id','main');
+			$('#main').removeAttr('style');
+		});
+	});
+}
+
+function loadFromRight(page) {
+	var pos_x = $(window).width();
+	var sliderdiv = $('<div id="slider" style="left:' + pos_x + 'px"></div>');
+	sliderdiv.load(page,function() {
+		$('#main').after(sliderdiv);
+		$('#main').animate({
+			left:'-=' + $(window).width()
+		},800);
+		$('#slider').animate({
+			opacity:'1.0',
+			left:'-=' + $(window).width()
+		},800,function() {
+			$('#main').remove();
+			$('#slider').attr('id','main');
+			$('#main').removeAttr('style');
+		});
+	});
 }
 
 function updateNavigation() {
